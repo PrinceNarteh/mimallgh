4ae0911ff99cb54f11394d1553f3b8be1f4848d6
Prince Narteh <prinarteh@gmail.com>
2023-03-03 15:03:49 +0000

    refactor more cards

--- mimall/src/pages/more.tsx
+++ mimall/src/pages/more.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const MoreProducts = () => {
@@ -17,16 +18,32 @@ const MoreProducts = () => {
               {Array(4)
                 .fill(0)
                 .map(() => (
-                  <div className="relative h-96 rounded-md bg-gray-500 bg-opacity-20 p-5">
-                    <div className="flex items-center justify-between">
+                  <div className="relative flex h-96 flex-col rounded-md bg-white shadow-md">
+                    <div className="flex gap-2 p-2">
                       <span className="block h-12 w-12 rounded-full bg-[#ff0000]"></span>
-                      <span className="block text-xl text-gray-700 line-through">
-                        $120.00
-                      </span>
+                      <div>
+                        <h4 className="font-semibold">The Web Super Market</h4>
+                        <p className="text-sm">Old Site - Behind ATL Hall</p>
+                      </div>
                     </div>
-                    <span className="absolute bottom-5 text-xl font-bold text-orange-500">
-                      $105.00
-                    </span>
+                    <div className="relative flex-1 rounded-md bg-gray-500 bg-opacity-20 p-5">
+                      <Image src="/images/iphone-2.jpg" fill alt="" />
+                    </div>
+                    <div className="flex h-16 gap-3 p-2">
+                      <div className="">
+                        <h4 className="font-semibold line-clamp-1">
+                          Lorem, ipsum dolor sit amet elit sunt.
+                        </h4>
+                        <p>Store price: ¢120.50</p>
+                      </div>
+                      <div className="flex w-28 items-center justify-center">
+                        <p className="flex items-start tracking-widest text-[#ff0000] ">
+                          <span className="text-xl">¢</span>
+                          <span className="text-4xl">123</span>
+                          <span className="text-xl">.50</span>
+                        </p>
+                      </div>
+                    </div>
                   </div>
                 ))}
             </div>
