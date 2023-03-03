07f86efc3601327d43f95e3808e0a07aa1b1db7e
Prince Narteh <prinarteh@gmail.com>
2023-03-03 07:13:34 +0000

    fix sub navbar responsiveness

--- mimall/src/components/layout/SubNavbar.tsx
+++ mimall/src/components/layout/SubNavbar.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { BiMenu } from "react-icons/bi";
 import { locations } from "../../utils/menus";
 import NavItem from "./NavItem";
+import { CgMenuRightAlt } from "react-icons/cg";
 
 const menus = [
   {
@@ -24,6 +25,7 @@ const menus = [
 
 const SubNavbar = () => {
   const [open, setOpen] = useState(false);
+  const [openHelp, setOpenHelp] = useState(false);
   const menuRef = useRef<any>(null);
   const btnRef = useRef<any>(null);
 
@@ -42,8 +44,10 @@ const SubNavbar = () => {
     };
   });
 
+  console.log(openHelp);
+
   return (
-    <div className="bg-gray-300">
+    <div className="bg-gray-300 px-2">
       <div className="relative mx-auto flex max-w-7xl justify-between py-3">
         <div
           className="flex cursor-pointer items-center space-x-2"
@@ -59,7 +63,7 @@ const SubNavbar = () => {
           ${
             open
               ? "visible translate-y-0 opacity-100"
-              : "invisible -translate-y-2 opacity-0"
+              : "invisible translate-y-2 opacity-0"
           }
         `}
         >
@@ -74,7 +78,32 @@ const SubNavbar = () => {
             </div>
           ))}
         </div>
-        <div>
+        <CgMenuRightAlt
+          onClick={() => setOpenHelp(!openHelp)}
+          className="text-2xl md:hidden"
+        />
+        <div
+          // ref={menuRef}
+          className={`absolute top-12 right-0.5 z-10 w-full max-w-fit border bg-gray-300 py-2 text-gray-800 shadow-md duration-500
+          ${
+            openHelp
+              ? "visible translate-y-0 opacity-100"
+              : "invisible translate-y-2 opacity-0"
+          }
+        `}
+        >
+          {menus.map((menu, idx) => (
+            <div
+              key={idx}
+              className={`cursor-pointer px-7 py-2 duration-500 hover:bg-[#ff0000] hover:text-white
+              ${openHelp ? "opacity-100" : "opacity-0"}
+              `}
+            >
+              {menu.label}
+            </div>
+          ))}
+        </div>
+        <div className="hidden md:block">
           <ul className="flex divide-x divide-gray-500">
             {menus.map((menu, index) => (
               <NavItem key={index} label={menu.label} link={menu.link} />
