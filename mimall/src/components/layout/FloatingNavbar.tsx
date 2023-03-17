859fc2d788eb4ebe5c94a40d0c0e79557af29ad1
Prince Narteh <prinarteh@gmail.com>
2023-03-17 10:09:46 +0000

    add styling to floating navbar

--- mimall/src/components/layout/FloatingNavbar.tsx
+++ mimall/src/components/layout/FloatingNavbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 
 const FloatingNavbar = ({ show }: { show: boolean }) => {
   console.log(show);
@@ -6,13 +7,21 @@ const FloatingNavbar = ({ show }: { show: boolean }) => {
   return (
     <div
       className={`${
-        show ? "fixed block" : "hidden"
-      }  top-0 z-50 w-full bg-red-900 px-5 duration-200`}
+        show ? " block translate-y-0" : "-translate-y-20"
+      } fixed top-0 z-50 flex w-full transform items-center bg-white px-10 duration-200`}
     >
-      <div
-        className={"mx-auto flex max-w-7xl items-center justify-between py-2"}
-      >
-        <h3 className="text-3xl text-pink-500">MiMall</h3>
+      <div className={"flex w-full items-center py-2"}>
+        <h3 className="mr-20 text-3xl text-pink-500">MiMall</h3>
+        <div className="flex w-full justify-center">
+          <div className="relative flex w-full max-w-2xl items-center justify-center rounded-full border-2 bg-white px-4">
+            <input
+              type="text"
+              className="flex-1 py-1 px-2 outline-none"
+              placeholder="Search for product..."
+            />
+            <BiSearch className="w-10 shrink-0 text-3xl text-gray-500" />
+          </div>
+        </div>
       </div>
     </div>
   );
