80ae7e34eaed3501e8f1c66c87ee375777ad8a5d
Prince Narteh <prinarteh@gmail.com>
2023-04-08 05:27:38 +0000

    add cart service handlers

---
+++ api/src/modules/carts/cart.service.ts
@@ -0,0 +1,35 @@
+import type { Cart } from "@prisma/client";
+import { db } from "../../utils/db.server";
+
+export const getCart = async (id: string) => {
+  return db.cart.findUnique({
+    where: { id },
+  });
+};
+
+export const getByUserId = async (id: string) => {
+  return db.cart.findFirst({
+    where: {
+      userId: id,
+    },
+  });
+};
+
+export const createCart = async (data: Cart) => {
+  return db.cart.create({
+    data,
+  });
+};
+
+export const updateCart = async (id: string, data: Cart) => {
+  return db.cart.update({
+    where: { id },
+    data,
+  });
+};
+
+export const deleteCart = async (id: string) => {
+  return db.cart.delete({
+    where: { id },
+  });
+};
