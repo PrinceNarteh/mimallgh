6b408b616e99c37a2496fdcdf550021ea4bb08c6
Prince Narteh <prinarteh@gmail.com>
2023-04-08 02:48:02 +0000

    add user service handler

---
+++ api/src/modules/users/user.service.ts
@@ -0,0 +1,43 @@
+import type { User } from "@prisma/client";
+import { db } from "../../utils/db.server";
+
+export const getUserById = async (id: string): Promise<User | null> => {
+  return db.user.findUnique({
+    where: {
+      id,
+    },
+  });
+};
+
+export const getUserByEmailOrPhoneNumber = async (
+  emailOrPhoneNumber: string
+) => {
+  return db.user.findFirst({
+    where: {
+      OR: [{ email: emailOrPhoneNumber }, { phoneNumber: emailOrPhoneNumber }],
+    },
+  });
+};
+
+export const createUser = async (input: User) => {
+  return db.user.create({
+    data: input,
+  });
+};
+
+export const editUser = async (id: string, input: User) => {
+  return db.user.update({
+    where: {
+      id,
+    },
+    data: input,
+  });
+};
+
+export const deleteUser = async (id: string, input: User) => {
+  return db.user.delete({
+    where: {
+      id,
+    },
+  });
+};
