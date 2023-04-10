3b1ecb73dbb11d22b1244df2e4572a501dc0a850
Prince Narteh <prinarteh@gmail.com>
2023-04-10 18:09:19 +0000

    add update and delete cart handlers

--- api/src/modules/carts/cart.controller.ts
+++ api/src/modules/carts/cart.controller.ts
@@ -22,3 +22,28 @@ export const getCartByUser = async (req: Request, res: Response) => {
     return res.status(500).json(error.message);
   }
 };
+
+export const updateCart = async (req: Request, res: Response) => {
+  const { cartId } = req.params;
+  const { body } = req;
+  try {
+    let cart = await CartService.getCart(cartId);
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+    cart = await CartService.updateCart(cartId, body);
+    res.status(200).json({ cart });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export const deleteCart = async (req: Request, res: Response) => {
+  const { cartId } = req.params;
+  try {
+    CartService.deleteCart(cartId);
+    res.status(200).json({ message: "Cart Delete successfully" });
+  } catch (error: any) {
+    return res.status(500).json(error.message);
+  }
+};
